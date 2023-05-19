d3b632593e4e541b0e3d28b38c5090cf36d40669
Youssef ( <yuossefbakier@gmail.com>
2023-05-19 09:23:59 -0700

    Add STANDER Files

---
+++ stander_header.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+								/*HEADERS*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+								/*DEFINATIONS*/
+#define TRUE 1
+
+								/*PROTOTYPE*/
+void ready(char *argv[], char **envr);
+
+
+#endif
---
+++ stander_main.c
@@ -0,0 +1,21 @@
+#include "stander_header.h"
+
+/**
+ * main - the main function
+ *
+ * Parameter:
+ * @argc: argument count
+ * @argv: array of argument
+ * @envr: envionment vector
+ *
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[], char **envr)
+{
+	if (argc == 1)
+		ready(argv, envr);
+	else
+		return (1);
+	return (0);
+}
