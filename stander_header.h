f6572304650fe338fccb9ed7641a60ed4fe4d3f9
Muhammad Ruby Muhammad <93392046+MuhammadRuby@users.noreply.github.com>
2023-05-24 14:18:39 +0300

    Update stander_header.h

--- stander_header.h
+++ stander_header.h
@@ -12,12 +12,15 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-
 								/*DEFINATIONS*/
 #define TRUE 1
 
 								/*PROTOTYPE*/
 void ready(char *argv[], char **envr);
 char **_strtok(char *buffer, int size);
+int set_shell_env(const char *variable, const char *value);
+int unset_shell_env(const char *variable);
+int execute_builtin_command(char **args);
+void shell_loop();
 
 #endif
